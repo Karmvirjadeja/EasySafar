90181b8e5c52035aa63e5508a906da29f16cff7f
Karmvir <jadejakarmvir12@gmail.com>
2024-10-04 16:53:03 +0530

    Here completed the infsection

--- src/view-trip/InfoSection.jsx
+++ src/view-trip/InfoSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
+import { IoMdDownload } from "react-icons/io";
 const InfoSection = () => {
   const tripData = JSON.parse(localStorage.getItem("TripData"));
   console.log(typeof tripData);
@@ -12,9 +13,24 @@ const InfoSection = () => {
         alt="Aeroplane"
         className="h-[340px] w-full object-cover rounded-xl"
       />
-
-      <div>
-        <h2>{hotels[0].description}</h2>
+      <div className="flex justify-between items-center">
+        <div className="my-5 flex flex-col gap-2">
+          <h2 className="font-bold text-2xl">{hotels[0].description}</h2>
+          <div className="flex gap-5">
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-lg">
+              📅No of days selected by user
+            </h2>
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-lg">
+              💰budget
+            </h2>
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-lg">
+              🥂No of travellers
+            </h2>
+          </div>
+        </div>
+        <Button>
+          <IoMdDownload />
+        </Button>
       </div>
     </div>
   );
