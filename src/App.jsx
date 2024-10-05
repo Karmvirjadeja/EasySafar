587c730f3edf017b2aa94aae070b17385eacfeb2
Karmvir <jadejakarmvir12@gmail.com>
2024-10-05 11:31:27 +0530

    Here fixed an import bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Hero from "@/components/custom/Hero";
+import Hero from "@/components/custom/Hero.jsx";
 
 function App() {
   return (
